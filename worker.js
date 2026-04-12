26ceda3f9f3a3b7c4b8cd356fe00241fd569730f
Suhani00796 <ss7330@srmist.edu.in>
2026-04-12 19:00:01 +0530

    Best practice: Remove skipWaiting from install event - preserve user session state

--- worker.js
+++ worker.js
@@ -19,7 +19,6 @@ self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then((cache) => cache.addAll(STATIC_ASSETS))
-            .then(() => self.skipWaiting())
             .catch((error) => console.error('Service Worker: Installation failed:', error))
     );
 });
